de6fb45666d0d1bb290ced8609beac27cd21dfca
jmonta01 <jason@montydigital.com>
2014-05-10 17:06:21 -0500

    adding in 30 second reload interval on now playing song display

--- app/scripts/controllers/now-playing.js
+++ app/scripts/controllers/now-playing.js
@@ -1,8 +1,10 @@
 'use strict';
 
 angular.module('somafmPlayerApp')
-    .controller('NowPlayingCtrl', ['$scope', '$log', 'StationService',
-        function ($scope, $log, StationService) {
+    .controller('NowPlayingCtrl', ['$scope', '$rootScope', '$log', '$interval', 'StationService',
+        function ($scope, $rootScope, $log, $interval, StationService) {
+
+            var songsP;
 
             $scope.playList = [];
 
@@ -20,6 +22,19 @@ angular.module('somafmPlayerApp')
             $scope.toggleFavSong = function (song) {
                 song.favorite = !song.favorite;
             };
+
+            $rootScope.$watch('selectedStation', function (station) {
+                console.log("selected station changed", station);
+                if (station === null) {
+                    $interval.cancel(songsP);
+                    songsP = undefined;
+                } else {
+                    songsP = $interval(function () {
+                        $scope.getPlayList(station);
+                    }, 30000);
+                }
+                //
+            });
 
         }
     ]);
