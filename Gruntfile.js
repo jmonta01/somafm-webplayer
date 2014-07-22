9f8668e5faa1bcc5c1289f9a0fe511deceacc7eb
Jason Montalto <jason@montydigital.com>
2014-07-22 00:43:45 -0500

    fixing build issues from moving bootstrap into project

--- Gruntfile.js
+++ Gruntfile.js
@@ -68,7 +68,7 @@ module.exports = function (grunt) {
             options: {
                 port: 9000,
                 // Change this to '0.0.0.0' to access the server from outside.
-                hostname: '0.0.0.0',
+                hostname: 'localhost',
                 livereload: 35729
             },
             livereload: {
@@ -332,8 +332,8 @@ module.exports = function (grunt) {
                         '*.html',
                         'views/{,*/}*.html',
                         'images/*.{webp,png,jpg,jpeg,gif}',
-                        'fonts/*',
-                        'data/*',
+                        'styles/fonts/*',
+                        //'data/*',
                         'bower_components/bootstrap-sass-official/vendor/assets/fonts/bootstrap/*.*'
 
                     ]
