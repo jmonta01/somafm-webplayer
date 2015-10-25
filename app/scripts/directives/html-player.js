179802a500160f6ece67f1316b778bc38cc45bab
Jason Montalto <jasonmontalto@Jasons-MBP.T-mobile.com>
2015-10-24 18:30:38 -0600

    Fixing bug when umuting with new sticky volume controls. Replaced pause in player controls with play/stop. When pausing for long times, the stream losses its buffer and will stop playing anything.

--- app/scripts/directives/html-player.js
+++ app/scripts/directives/html-player.js
@@ -13,41 +13,26 @@ angular.module('somafmPlayerApp')
           "<div class='player'>" +
           "<audio id='audioPlayer' autoplay='true' preload='none'></audio>" +
           "<button id='playBtn' class='btn btn-link btn-lg' ng-show='!playing' ng-disabled='!station' ng-click='togglePlay()'><span class='glyphicon glyphicon-play'></span></button>" +
-          "<button id='pauseBtn' class='btn btn-link btn-lg' ng-show='playing' ng-disabled='!station' ng-click='togglePlay()'><span class='glyphicon glyphicon-pause'></span></button>" +
+          "<button id='stopBtn' class='btn btn-link btn-lg' ng-show='playing' ng-disabled='!station' ng-click='togglePlay()'><span class='glyphicon glyphicon-stop'></span></button>" +
           "<button id='unmuteBtn' class='btn btn-link btn-lg' ng-show='!isMuted()' ng-click='toggleMute()'><span class='glyphicon glyphicon-volume-down'></span></button>" +
           "<button id='muteBtn' class='btn btn-link btn-lg' ng-show='isMuted()' ng-click='toggleMute()'><span class='glyphicon glyphicon-volume-off'></span></button>" +
           "<volumebar id='volumeslider' value='volume' min='0' max='1' value-change='updateVolume(val)'></volumebar>" +
           "<button id='maxvolBtn' class='btn btn-link btn-lg' ng-click='maxVolume()'><span class='glyphicon glyphicon-volume-up'></span></button>" +
           "</div>",
         link: function (scope, element, attr) {
-          var volumeKey = 'volume';
+          var volumeKey = 'volume', lastVolume, stationData = null;
 
           scope.muted = false;
           scope.playing = false;
 
           scope.audio = document.getElementById("audioPlayer");
-          scope.volume = StorageService.get(volumeKey) || 0.5;
 
           scope.$watch("station", function (val) {
-            scope.audio.pause();
-            angular.element(scope.audio).html("");
+            scope.stop();
 
-            if (val) {
-              angular.forEach(val.urls.reverse(), function (url) {
-                var source = document.createElement('source');
-                if (scope.audio.canPlayType('audio/mpeg;')) {
-                  source.type = 'audio/mpeg';
-                  source.src = url;
-                } else {
-                  source.type = 'audio/ogg';
-                  source.src = url;
-                }
-                scope.audio.appendChild(source);
-              });
-
-              scope.audio.load();
-              scope.audio.play();
-              scope.playing = true;
+            stationData = val;
+            if (stationData) {
+              scope.play();
             } else {
               scope.audio.load();
               scope.playing = false;
@@ -69,7 +54,7 @@ angular.module('somafmPlayerApp')
             if (!scope.playing) {
               totalWidth -= parseInt(angular.element(document.getElementById('playBtn')).css('width').replace('px', ''));
             }   else {
-              totalWidth -= parseInt(angular.element(document.getElementById('pauseBtn')).css('width').replace('px', ''));
+              totalWidth -= parseInt(angular.element(document.getElementById('stopBtn')).css('width').replace('px', ''));
             }
 
             if (!scope.isMuted()) {
@@ -84,27 +69,47 @@ angular.module('somafmPlayerApp')
             angular.element(document.getElementById('volumeslider')).css('width', Math.min(totalWidth, 200) + 'px');
           };
 
+          scope.play = function () {
+            angular.forEach(stationData.urls.reverse(), function (url) {
+              var source = document.createElement('source');
+              if (scope.audio.canPlayType('audio/mpeg;')) {
+                source.type = 'audio/mpeg';
+                source.src = url;
+              } else {
+                source.type = 'audio/ogg';
+                source.src = url;
+              }
+              scope.audio.appendChild(source);
+            });
+
+            scope.audio.load();
+            scope.audio.play();
+            scope.playing = true;
+          };
+
+          scope.stop = function () {
+            scope.audio.pause();
+            scope.currentTime = 0;
+            scope.playing = false;
+
+            angular.element(scope.audio).html("");
+          };
+
           scope.togglePlay = function () {
             if (scope.playing) {
-              scope.audio.pause();
-              scope.playing = false;
+              scope.stop();
             } else {
-              scope.audio.play();
-              scope.playing = true;
+              scope.play();
             }
           };
 
           scope.maxVolume = function () {
-            if (scope.audio.muted) {
-              scope.audio.muted = false;
-            }
+            if (scope.audio.muted) scope.audio.muted = false;
             scope.setVolume(1);
           };
 
           scope.updateVolume = function (value) {
-            if (scope.audio.muted) {
-              scope.audio.muted = false;
-            }
+            if (scope.audio.muted) scope.audio.muted = false;
             scope.setVolume(value);
           };
 
@@ -116,7 +121,8 @@ angular.module('somafmPlayerApp')
           scope.toggleMute = function () {
             scope.muted = !scope.muted;
             scope.audio.muted = scope.muted;
-            scope.volume = scope.muted ? 0 : scope.audio.volume;
+            if (scope.muted)  lastVolume = scope.audio.volume;
+            scope.setVolume(scope.muted ? 0 : lastVolume);
           };
 
           scope.isMuted = function () {
@@ -124,6 +130,7 @@ angular.module('somafmPlayerApp')
           };
 
           scope.updateLayout();
+          scope.setVolume(StorageService.get(volumeKey) || 0.5);
         }
       }
     }
