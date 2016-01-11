d6a821b50dbc7ddbdce18fd800060675a9fa7c85
Jason Montalto <jasonmontalto@Jasons-MBP.lan>
2016-01-11 00:12:34 -0700

    start of styling of updated facebook widget

--- app/scripts/community/directives/views/facebook.js
+++ app/scripts/community/directives/views/facebook.js
@@ -10,7 +10,16 @@ angular.module('somafmPlayerApp')
         scope: {},
         templateUrl: 'community/views/facebook.tpl.html',
         link: function (scope, element, attr) {
-          element.append('<iframe src="http://somafm.com/app/facebook.html"></iframe>');
+          //element.append('<iframe src="http://somafm.com/app/facebook.html"></iframe>');
+
+          var showWidget = function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5";
+            fjs.parentNode.insertBefore(js, fjs);
+          };
+          showWidget(document, 'script', 'facebook-jssdk');
         }
       }
     }
